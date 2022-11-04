b6e59181dfdf19cc194a23123675f07afad90507
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2022-11-04 11:07:53 +0530

    Added Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <p>Home Page</p>
     </div>
   );
 }
-
-export default App;
