f970a766d9760bb11f012398316eb0a45c1a722e
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2022-11-11 18:32:59 +0530

    Added comments and updated favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Container from "../components/Container";
 import { songdata } from "../components/songdata";
+import {
+  Play,
+  Pause,
+  Repeat,
+  SkipForward,
+  SkipBack,
+  Shuffle,
+} from "react-feather";
 
 export default function Home() {
   // Getting last song listented to by User
   let getUserMusic = JSON.parse(localStorage.getItem("song"));
   // Setting up Looping usestate
   const [isLooping, setIsLooping] = useState(false);
+  // Setting up Shuffling usestate
   const [isShuffle, setIsShuffle] = useState(false);
+  // Finding duration of current song
   const [duration, setDuration] = useState(0);
+  // Timestamp of current song
   const [currentTime, setCurrentTime] = useState(0);
+  // Using mounted to give useEffect a way to know if it is mounted
   const [mounted, setMounted] = useState(false);
+  // Getting element audio
   const music = document.getElementById("audio");
+  // Getting current song
   let [currentlyPlaying, setCurrentlyPlaying] = useState(getUserMusic);
+  // Checking if song is playing
   const [isPlaying, setIsPlaying] = useState(false);
   const nextSong = () => {
+    // Going to next song given shuffling is false
     if (!isShuffle) {
       setCurrentlyPlaying(
         currentlyPlaying >= songdata.length - 1 ? 0 : currentlyPlaying + 1
@@ -25,7 +41,7 @@ export default function Home() {
       );
     } else {
       let randomSong = Math.floor(Math.random() * songdata.length);
-      if (randomSong === currentlyPlaying) {
+      while (randomSong === currentlyPlaying) {
         randomSong = Math.floor(Math.random() * songdata.length);
       }
       setCurrentlyPlaying(randomSong);
@@ -70,28 +86,43 @@ export default function Home() {
           }}
         ></audio>
         {mounted && (
-          <div className="border-4 flex flex-col items-center">
-            <p>{songdata[currentlyPlaying].id} current id</p>
-            <p>{songdata[currentlyPlaying].name} current id name</p>
-            <div className="flex flex-row items-center justify-center">
-              <span id="current-time">
-                {Math.floor(currentTime / 60) || "-"}
-              </span>
+          <div className="border-4 flex flex-col items-center min-w-[300px]">
+            <p>
+              Playing {parseInt(songdata[currentlyPlaying].id) + 1} of{" "}
+              {songdata.length}
+            </p>
+            <p>{songdata[currentlyPlaying].name} </p>
+            <div className="flex flex-row items-center justify-center w-full">
               <input
                 type="range"
                 min={0}
                 max={duration}
                 value={currentTime}
-                className="mx-5 w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-700"
+                className="mx-5 w-[200px] h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-700"
                 onChange={(e) => {
                   music.currentTime = e.target.value / 100;
                 }}
               />
               <span id="full-time"></span>
+            </div>
+            <div className="flex flex-row justify-between min-w-[200px] mt-1">
+              <span id="current-time" className="px-2">
+                {Math.floor(currentTime / 60) || "-"}
+              </span>
               <p>{Math.floor(duration / 60) || "-"}</p>
             </div>
-            <div className="flex flex-row items-center justify-center">
-              <button onClick={prevSong}>Previous Song</button>
+            <div className="flex flex-row items-center justify-center w-full">
+              <button
+                onClick={() => {
+                  setIsLooping(!isLooping);
+                }}
+                className=""
+              >
+                {isLooping ? <Repeat className="text-blue-500" /> : <Repeat />}
+              </button>
+              <button onClick={prevSong} className="ml-4">
+                <SkipBack />
+              </button>
               <button
                 onClick={() => {
                   setIsPlaying(!isPlaying);
@@ -101,28 +132,24 @@ export default function Home() {
                     music.play();
                   }
                 }}
-                className="mx-5"
+                className="mx-4"
               >
-                {isPlaying ? "Pause" : "Play"}
+                {isPlaying ? <Pause /> : <Play />}
               </button>
-              <button onClick={nextSong}>Next Song</button>
-            </div>
-            <div>
-              <button
-                onClick={() => {
-                  setIsLooping(!isLooping);
-                }}
-                className="ml-10"
-              >
-                {isLooping ? "LoopTrue" : "LoopFalse"}
+              <button onClick={nextSong} className="mr-4">
+                <SkipForward />
               </button>
               <button
                 onClick={() => {
                   setIsShuffle(!isShuffle);
                 }}
-                className="ml-10"
+                className=""
               >
-                {isShuffle ? "ShuffleTrue" : "ShuffleFalse"}
+                {isShuffle ? (
+                  <Shuffle className="text-blue-500" />
+                ) : (
+                  <Shuffle />
+                )}
               </button>
             </div>
           </div>
