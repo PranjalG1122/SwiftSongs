d84934bbc6cd18b928682d7a22da325d2b03693f
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2022-11-17 22:10:22 +0530

    Updated Songs

--- src/components/songdata.js
+++ src/components/songdata.js
@@ -1,7 +1,27 @@
 export const songdata = [
-  { id: "0", source: "/song0.mp3", name: "Young Hearts" },
-  { id: "1", source: "/song1.mp3", name: "NCS" },
-  { id: "2", source: "/song2.mp3", name: "Everybody is Interesting" },
-  { id: "3", source: "/song3.mp3", name: "Industry" },
-  { id: "4", source: "/song4.mp3", name: "Endless" },
+  {
+    id: "0",
+    source:
+      "/Aeden & Sketchez - Take It or Leave It [NCS Release]-mkHOFbc0PKA-160k-1654379631158.mp3",
+    name: "Take It or Leave It",
+  },
+  { id: "1", source: "/Lets Take it Slow.mp3", name: "Lets Take it Slow" },
+  {
+    id: "2",
+    source:
+      "/Culture Code - Not Giving In [NCS Release]-R_NsDmpu-Wc-160k-1658366683214.mp3",
+    name: "Not Giving In",
+  },
+  {
+    id: "3",
+    source:
+      "/ReauBeau - Wizkid feat. AVA NOVA [NCS Release]-q6kzYPfhilg-160k-1654054938780.mp3",
+    name: "Wizkid",
+  },
+  {
+    id: "4",
+    source:
+      "/Unknown Brain - Superhero (feat. Chris Linton) [NCS Release]-LHvYrn3FAgI-160k-1654355909190.mp3",
+    name: "Superhero",
+  },
 ];
