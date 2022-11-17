0d481ae3edfd043736fa279d2bba510c76fd1847
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2022-11-17 22:18:49 +0530

    Updated 404 Page

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -7,8 +7,17 @@ export default function Page404() {
       <Helmet>
         <title>404</title>
       </Helmet>
-      <div className="flex flex-col justify-center items-cen">
-        <p>404</p>
+      <div className="flex flex-col justify-center items-center h-screen px-4">
+        <p className="text-6xl">404</p>
+        <p className="text-base tablet:text-xl my-4 text-gray-600">
+          How'd you get here?
+        </p>
+        <a
+          href="/"
+          className="text-xl bg-emerald-700 px-4 py-1 rounded transition-all hover:bg-emerald-500"
+        >
+          Home
+        </a>
       </div>
     </div>
   );
